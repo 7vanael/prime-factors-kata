e15e1c5adaa6328ea87efee6db5bc7cd02ce921d
7vanael <kristi.n.norris777@gmail.com>
2025-02-25 10:27:52 -0700

    init

---
+++ spec/prime_factors_kata/core_spec.clj
@@ -0,0 +1,7 @@
+(ns prime-factors-kata.core-spec
+  (:require [speclj.core :refer :all]
+            [prime-factors-kata.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/prime_factors_kata/core.clj
@@ -0,0 +1,5 @@
+(ns prime-factors-kata.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
