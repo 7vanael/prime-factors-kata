1eec596f32ef82b566213115727e2c3830869f4a
7vanael <kristi.n.norris777@gmail.com>
2025-02-25 15:57:03 -0700

    Kata complete

--- spec/prime_factors_kata/core_spec.clj
+++ spec/prime_factors_kata/core_spec.clj
@@ -2,6 +2,12 @@
   (:require [speclj.core :refer :all]
             [prime-factors-kata.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Prime Factors Kata"
+  (it "Finds the prime factors"
+    (should= [] (find-prime-factors 1))
+    (should= [2] (find-prime-factors 2))
+    (should= [3] (find-prime-factors 3))
+    (should= [2 2] (find-prime-factors 4))
+    (should= [2 3] (find-prime-factors 6))
+    (should= [2 2 2] (find-prime-factors 8))
+    (should= [3 3] (find-prime-factors 9))))
--- src/prime_factors_kata/core.clj
+++ src/prime_factors_kata/core.clj
@@ -3,3 +3,9 @@
 (defn -main
   [& args]
   (println "Hello World"))
+
+(defn find-prime-factors [n]
+  (loop [n n primes [] factor 2]
+    (cond (<= n 1) primes
+      (= 0 (rem n factor)) (recur (/ n factor) (conj primes factor) factor)
+      :else (recur n primes (inc factor)))))
