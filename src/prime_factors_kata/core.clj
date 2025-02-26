b1617dc681d4daeb0666016f6d9dcb74ff935de9
7vanael <kristi.n.norris777@gmail.com>
2025-02-25 19:46:59 -0700

    concise

--- src/prime_factors_kata/core.clj
+++ src/prime_factors_kata/core.clj
@@ -1,11 +1,8 @@
 (ns prime-factors-kata.core)
 
-(defn -main
-  [& args]
-  (println "Hello World"))
 
 (defn find-prime-factors [n]
   (loop [n n primes [] factor 2]
     (cond (<= n 1) primes
-      (= 0 (rem n factor)) (recur (/ n factor) (conj primes factor) factor)
-      :else (recur n primes (inc factor)))))
+        (= 0 (rem n factor)) (recur (/ n factor) (conj primes factor) factor)
+        :else (recur n primes (inc factor)))))
